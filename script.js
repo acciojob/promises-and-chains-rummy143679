10b1f2d905a03a15d8ff7bb8658a3e078323d21e
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-06-19 17:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const submitBtn = document.querySelector("#btn");
+const nameInp = document.querySelector("#name");
+const ageInp = document.querySelector("#age");
+
+submitBtn.addEventListener("click", () => {
+	if(nameInp.value.trim() == "" || ageInp.value == ""){
+		alert("Plese enter valid details")
+	}else{
+		const promis = new promise((resolve, reject) => {
+			setTimeout(() => {
+				if(ageInp.value > 18){
+					resolve(`Wellcome, ${nameInp.value}. You can vote.`);
+				}else{
+					reject(`Oh sorry ${nameInp.value}. You arent old enough.`);
+				}
+			}, 4000);
+			alert(promis);
+		})
+	}
+})
